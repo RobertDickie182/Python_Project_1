d16b7fc035dfda91f0bebd0bc4fba0004fc880f6
RobertDickie182 <robertdanieldickie@gmail.com>
2023-06-26 13:46:09 +0100

    add country function working

--- controllers/country_controller.py
+++ controllers/country_controller.py
@@ -7,7 +7,7 @@ import repositories.city_repository as city_repository
 
 countries_blueprint = Blueprint("countries", __name__)
 
-@countries_blueprint.route("/countries")
+@countries_blueprint.route("/countries", methods=["GET"])
 def users():
     countries = country_repository.select_all()
     return render_template("countries.html", countries = countries)
@@ -16,3 +16,22 @@ def users():
 def show(id):
     country = country_repository.select(id)
     return render_template("/countries/show.html", country=country)
+
+@countries_blueprint.route("/countries/new", methods=["GET"])
+def new_task():
+    return render_template("/countries/new_country.html")
+
+@countries_blueprint.route("/countries", methods=["POST"])
+def create():
+    name = request.form["name"]
+    country = Country(name)
+    country_repository.save(country)
+    return redirect("/countries")
+
+@countries_blueprint.route("countries/<id>/delete", methods=["POST"])
+def delete(id):
+    country_repository.delete(id)
+    return redirect("/countries")
+    
+
+
